1025231ad6607297dabbd7eee9f11f2cd75c95d4
Yvictor <fate2314@gmail.com>
2017-02-11 09:57:06 +0000

    amazon page test

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,15 @@
 # Created on Dec, 8, 2016
 # @author: Yvictor
 
-FROM ubuntu:16.04
+FROM yvictor/docker_conda:cmpy
 MAINTAINER yvictor
 
 # install python3.5 and essential
-RUN apt-get update && apt-get install -y python3.5 python-dev wget cron build-essential
+# RUN apt-get update && apt-get install -y python3.5 python-dev wget cron build-essential
 # install pip
-RUN wget https://bootstrap.pypa.io/get-pip.py -O- | python
+# RUN wget https://bootstrap.pypa.io/get-pip.py -O- | python
 # install python-lxml
-RUN apt-get update && apt-get install -y python-lxml
+# RUN apt-get update && apt-get install -y python-lxml
 
 
 COPY . /IPscan
--- pyipscan.py
+++ pyipscan.py
@@ -19,3 +19,24 @@ ip = [a.text for a in soup.select('#section_left a')][0]
 
 client = Client(dsn = SENTRY_DNS, name = DOCKER_NAME)
 client.captureMessage(ip)
+
+def _get_session():
+    session = requests.session()
+    headers = {
+        "referer": "https://www.amazon.com/",
+        "user-agent": "mozilla/5.0 (x11; linux x86_64) applewebkit/537.36 "
+                      "(khtml, like gecko) "
+                      "chrome/46.0.2490.86 safari/537.36"}
+    session.headers.update(headers)
+    return session
+
+def _get_product_url(product_id):
+    return 'https://www.amazon.com/dp/' + product_id
+
+pid = "B00XQ4Z1Q0"
+url = _get_product_url(pid)
+res = session.get(url)
+soup = BS(res.text, "lxml")
+print(soup.select("#dp-container")[0].text)
+print("===================================")
+print(soup.text)
